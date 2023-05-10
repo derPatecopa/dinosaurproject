8c78a38019500721c9ceb27e3a106be07ddeb561
derPatecopa <mb89@me.com>
2023-05-10 12:34:30 +0200

    added human object

--- app.js
+++ app.js
@@ -12,6 +12,26 @@ function Dino(species, weight, height, diet, where, when, fact) {
 // Create Dino Objects
 
 // Create Human Object
+const form = document.querySelector("#dino-compare");
+const compareButton = document.querySelector("#btn");
+compareButton.addEventListener("click", () => {
+    //IIFE
+  (() => {
+    const name = document.querySelector("#name").value;
+    const feet = parseFloat(document.querySelector("#feet").value);
+    const inches = parseFloat(document.querySelector("#inches").value);
+    const weight = parseFloat(document.querySelector("#weight").value);
+    const diet = document.querySelector('#diet').value;
+
+    const human = {
+      name: name,
+      feet: feet,
+      inches: inches,
+      weight: weight,
+      diet: diet
+    };
+  })();
+});
 
 // Use IIFE to get human data from form
 
@@ -25,17 +45,16 @@ function Dino(species, weight, height, diet, where, when, fact) {
 // NOTE: Weight in JSON file is in lbs, height in inches.
 
 // Generate Tiles for each Dino in Array
-const grid = document.querySelector('#grid');
-const gridTile = document.createElement('div');
+const grid = document.querySelector("#grid");
+const gridTile = document.createElement("div");
 gridTile.innerHTML = `<img src='images/anklyosaurus.png'>
                     <h3>Test Tile</h3>`;
 // Add tiles to DOM
-grid.appendChild(gridTile);
+if (form.remove) {grid.appendChild(gridTile);}
 
 // Remove form from screen
-const form = document.querySelector('#dino-compare');
-const compareButton = document.querySelector('#btn');
-compareButton.addEventListener('click', function() {
-    form.remove();
-})
+compareButton.addEventListener("click", function () {
+  form.remove();
+});
+
 // On button click, prepare and display infographic
