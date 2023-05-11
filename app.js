fcb98138267d48a3256ff1ad9ad9bb406eace94d
derPatecopa <mb89@me.com>
2023-05-11 09:51:05 +0200

    created dino objects

--- app.js
+++ app.js
@@ -10,27 +10,59 @@ function Dino(species, weight, height, diet, where, when, fact) {
 }
 
 // Create Dino Objects
+fetch("dino.json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("test message");
+    const dinos = data.Dinos.map(
+      (dino) =>
+        new Dino(
+          dino.species,
+          dino.weight,
+          dino.height,
+          dino.diet,
+          dino.where,
+          dino.when,
+          dino.fact
+        )
+    );
+   console.log(dinos[0].diet);
+    // Use the dinos array to generate tiles for each Dino
+    // and add them to the grid
+  });
+
 
 // Create Human Object
+const grid = document.querySelector("#grid");
+//creating grid Div
+const gridTile = document.createElement("div");
+//filling a grid tile with content
+
 const form = document.querySelector("#dino-compare");
 const compareButton = document.querySelector("#btn");
 compareButton.addEventListener("click", () => {
-    //IIFE
+  //IIFE
   (() => {
     const name = document.querySelector("#name").value;
     const feet = parseFloat(document.querySelector("#feet").value);
     const inches = parseFloat(document.querySelector("#inches").value);
     const weight = parseFloat(document.querySelector("#weight").value);
-    const diet = document.querySelector('#diet').value;
+    const diet = document.querySelector("#diet").value;
 
     const human = {
       name: name,
       feet: feet,
       inches: inches,
       weight: weight,
-      diet: diet
+      diet: diet,
     };
+    gridTile.innerHTML = `<img src='images/human.png'>
+                   <h3>${human.name}</h3>`;
   })();
+  //removing the form
+  form.remove();
+  //creating the grid
+  grid.appendChild(gridTile);
 });
 
 // Use IIFE to get human data from form
@@ -45,16 +77,9 @@ compareButton.addEventListener("click", () => {
 // NOTE: Weight in JSON file is in lbs, height in inches.
 
 // Generate Tiles for each Dino in Array
-const grid = document.querySelector("#grid");
-const gridTile = document.createElement("div");
-// gridTile.innerHTML = `<img src='images/anklyosaurus.png'>
-//                     <h3>Test Tile</h3>`;
+
 // Add tiles to DOM
-//if (form.remove) {grid.appendChild(gridTile);}
 
 // Remove form from screen
-compareButton.addEventListener("click", function () {
-  form.remove();
-});
 
 // On button click, prepare and display infographic
